1ca2e36e9c8d2963ef19d575e70cab9ad7048980
Dennis <dgrijalva01@gmail.com>
2014-10-20 17:25:34 -0700

    iPhone application

---
+++ king-of-the-house/king-of-the-house/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  king-of-the-house
+//
+//  Created by Dennis Grijalva on 10/20/14.
+//  Copyright (c) 2014 Dennis Grijalva. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ king-of-the-house/king-of-the-house/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  king-of-the-house
+//
+//  Created by Dennis Grijalva on 10/20/14.
+//  Copyright (c) 2014 Dennis Grijalva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
