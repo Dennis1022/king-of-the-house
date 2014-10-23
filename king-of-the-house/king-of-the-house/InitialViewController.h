8b082624e099c206cb3f5a17a7737e0d902437f3
Dennis <dgrijalva01@gmail.com>
2014-10-22 22:11:36 -0700

    Added view controllers, startup image, and icon.

--- king-of-the-house/king-of-the-house/InitialViewController.h
+++ king-of-the-house/king-of-the-house/InitialViewController.h
@@ -1,13 +1,13 @@
 //
-//  ViewController.h
+//  InitialViewController.h
 //  king-of-the-house
 //
-//  Created by Dennis Grijalva on 10/20/14.
+//  Created by Dennis Grijalva on 10/22/14.
 //  Copyright (c) 2014 Dennis Grijalva. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface InitialViewController : UIViewController
 
 @end
---
+++ king-of-the-house/king-of-the-house/InitialViewController.m
@@ -0,0 +1,49 @@
+//
+//  InitialViewController.m
+//  king-of-the-house
+//
+//  Created by Dennis Grijalva on 10/22/14.
+//  Copyright (c) 2014 Dennis Grijalva. All rights reserved.
+//
+
+#import "InitialViewController.h"
+
+@interface InitialViewController ()
+
+@end
+
+@implementation InitialViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
